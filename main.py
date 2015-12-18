bfe32ff84a2a9aa9ccc3a34a84dbccbb4de3586c
marbiru <code@uribram.com>
2015-12-18 12:44:30 +0700

    Random selection from demo list
    program randomly selects items from demo list and correctly prints the
    previous and subsequent item for context

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-demo_list = ["He would declare and could himself believe", "that the birds there in all the garden round", "from having heard the daylong voice of Eve", "had added to their own an oversound", "her tone of meaning but without the words", "admittedly an eloquence so soft", "could only have had an influence on birds"]
+demo_list = ["He would declare and could himself believe", "that the birds there in all the garden round", "from having heard the daylong voice of Eve", "had added to their own an oversound", "her tone of meaning but without the words", "admittedly an eloquence so soft", "could only have had an influence on birds", "when call or laughter carried it aloft"]
 
-demo_line = "that the birds there in all the garden round"
+from random import randint
 
-demo_pre = "He would declare and could himself believe"
+random_number = randint(0,len(demo_list) - 1)
 
-demo_post = "from having heard the daylong voice of Eve"
+try:
+	pre_line = demo_list[random_number - 1]
+except IndexError:
+	pre_line ="START OF LIST"
 
-blank_filler = "_" * len(demo_line)
+try:
+	post_line = demo_list[random_number + 1]
+except IndexError:
+	post_line ="END OF LIST"
+
+blank_filler = "-" * len((demo_list[random_number]))
 
 print "\n"
-print demo_pre
+print pre_line
 print blank_filler
-print demo_post
+print post_line
 print "\n"
