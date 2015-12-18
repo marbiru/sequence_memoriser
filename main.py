1721653f3d6eff09adf3b385dd54e418c8d2b11d
marbiru <code@uribram.com>
2015-12-18 11:53:52 +0700

    Dummy Random-memoriser
    just prints a dummy list, but does it fine

---
+++ main.py
@@ -0,0 +1,15 @@
+demo_list = ["He would declare and could himself believe", "that the birds there in all the garden round", "from having heard the daylong voice of Eve", "had added to their own an oversound", "her tone of meaning but without the words", "admittedly an eloquence so soft", "could only have had an influence on birds"]
+
+demo_line = "that the birds there in all the garden round"
+
+demo_pre = "He would declare and could himself believe"
+
+demo_post = "from having heard the daylong voice of Eve"
+
+blank_filler = "_" * len(demo_line)
+
+print "\n"
+print demo_pre
+print blank_filler
+print demo_post
+print "\n"
